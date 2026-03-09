f0f547653128d7ade86b6ed9695e3342a6949269
Gagan Jain <gaganmahatma88@gmail.com>
2026-03-09 10:33:23 +0530

    Robot file fixes

--- app/robots.ts
+++ app/robots.ts
@@ -2,7 +2,8 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   const baseUrl =
-    process.env.NEXT_PUBLIC_SITE_URL || 'https://usecasepilot.com'
+    process.env.NEXT_PUBLIC_SITE_URL || 'https://usecasepilot.org'
+
   return {
     rules: [
       {
